7c04340340ea3e87db8af43179625dabd9362350
umangghoghari24 <125755486+umangghoghari24@users.noreply.github.com>
2023-06-30 13:05:02 +0530

    WhatsApp

--- lib/contact.dart
+++ lib/contact.dart
@@ -3,7 +3,11 @@ import 'package:umang2/Models/contactModal.dart';
 import 'package:umang2/addnumber.dart';
 import 'package:umang2/contact_no.dart';
 import 'package:umang2/listview2.dart';
+import 'package:umang2/personal%20sms.dart';
 import 'package:umang2/status.dart';
+import 'package:contacts_service/contacts_service.dart';
+import 'package:permission_handler/permission_handler.dart';
+
 
 class contact extends StatefulWidget {
   const contact({Key? key}) : super(key: key);
@@ -13,175 +17,129 @@ class contact extends StatefulWidget {
 }
 
 class _contactState extends State<contact> {
-  List<cotactModal> myfriends = [
-    cotactModal(
+  List<Contact> contacts=[];
+  bool isloaded= false;
 
-          title:'nirali',
-          image: 'https://media.istockphoto.com/id/1399395748/photo/cheerful-business-woman-with-glasses-posing-with-her-hands-under-her-face-showing-her-smile.jpg?s=2048x2048&w=is&k=20&c=48p2zoD9WxbKWhdY9-mp0fXcqVlx2I2VePgT3Rx7dPg='
+  void getContacts() async{
 
-      ),
-      cotactModal(
-
-          title:'ankita',
-          image: 'https://media.istockphoto.com/id/1416048929/photo/woman-working-on-laptop-online-checking-emails-and-planning-on-the-internet-while-sitting-in.webp?s=2048x2048&w=is&k=20&c=6omJSQtYXI93gG8zTc2aCJOCJrqBf-Bk8b_W-IXvy9s='
-      ),
-    cotactModal(
-        title: 'umang',
-        image:
-            'https://images.unsplash.com/photo-1544723795-3fb6469f5b39?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDN8fHBlcnNvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'),
-    cotactModal(
-        title: 'shail',
-        image:
-            'https://www.shutterstock.com/image-photo/young-handsome-man-beard-wearing-260nw-1768126784.jpg'),
-    cotactModal(
-        title: 'jay',
-        image:
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdtCoCXEsYJVATCZJj8_hvkP5SmEFQ-mEcNFLcPH6Hmg&s'),
-    cotactModal(
-        title: 'jaydeep',
-        image:
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTuYTPz8et6f08brD5MZAtt1ADZxgiKa3-0HwDFzyLYg&s'),
-    cotactModal(
-        title: 'pal',
-        image:
-            'https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'),
-  ];
-
+    if(await Permission.contacts.isGranted){
+      List<Contact> tmp_contacts = await ContactsService.getContacts( );
+      setState(() {
+        contacts=tmp_contacts;
+      });
+    }else{
+      Permission.contacts.request();
+    }
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getContacts();
+    setState(() {
+      isloaded = true;
+    });
+    Future.delayed(Duration(seconds: 10), () {
+      setState(() {
+        isloaded=false;
+      });
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Column(
-          children: [
-            SizedBox(
-              width: 150,
-              child: Text('Select contact'),
-            ),
-            SizedBox(
-              width: 150,
-              child: Text(
-                  style: TextStyle(fontSize: 13, fontWeight: FontWeight.normal),
-                  '200 contacts'),
-            ),
-          ],
-        ),
-        actions: [
-          SizedBox(
-              width: 50,
-              child: Icon(
-                Icons.search,
-                color: Colors.white,
-              )),
-      PopupMenuButton(
-                icon: Icon(Icons.more_vert),
-                itemBuilder: (context) => [
-                      PopupMenuItem(child: Text("Invite a friend")),
-                      PopupMenuItem(child: Text("Contacts")),
-                      PopupMenuItem(child: Text("Refresh")),
-                      PopupMenuItem(child: Text("Help")),
-                    ])
-        ],
-      ),
-      // appBar: AppBar(
-      //   title: Column(
-      //     mainAxisAlignment: MainAxisAlignment.start,
-      //     crossAxisAlignment: CrossAxisAlignment.start,
-      //     children: [
-      //       Text('select contact',
-      //       style: TextStyle(fontSize: 20),),
-      //       Text('200 contact'),
-      //     ],
-      //   ),
-      //   actions: [
-      //     IconButton(
-      //         onPressed: () {},
-      //         icon: Icon(
-      //           Icons.search,
-      //           size: 26,
-      //         )),
-      //     PopupMenuButton(
-      //         icon: Icon(Icons.more_vert),
-      //         itemBuilder: (context) => [
-      //               PopupMenuItem(child: Text("Invite a friend")),
-      //               PopupMenuItem(child: Text("Contacts")),
-      //               PopupMenuItem(child: Text("Refresh")),
-      //               PopupMenuItem(child: Text("Help")),
-      //             ])
-      //   ],
-      // ),
-      // body: SingleChildScrollView(
-      //   child: Column(
-      //     children: [
-      //       ListTile(
-      //         leading: CircleAvatar(
-      //           child: Icon(Icons.people),
-      //         ),
-      //         title: Text('New group'),
-      //         onTap: () {
-      //           Navigator.push(context,MaterialPageRoute(builder: (context)=>contactno()));
-      //         },
-      //       ),
-      //       ListTile(
-      //         leading: CircleAvatar(
-      //           child: Icon(Icons.person_add),
-      //         ),
-      //         title: Text('New contact'),
-      //       ),
-      //
-      //
-      //     ],
-      //   ),
-      // ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-                child: Column(
+          appBar: AppBar(
+            title: Column(
               children: [
-                ListTile(
-                  leading: CircleAvatar(
-                    child: Icon(Icons.people),
-                  ),
-                  title: Text('New group'),
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => contactno()));
-                  },
+                SizedBox(
+                  width: 150,
+                  child: Text('Select contact'),
                 ),
-                ListTile(
-                  leading: CircleAvatar(
-                    child: Icon(Icons.person_add),
-                  ),
-                  title: Text('New contact'),
-                  onTap: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=>addnumber()));
-                  },
+                SizedBox(
+                  width: 150,
+                  child: Text(
+                      style: TextStyle(fontSize: 13, fontWeight: FontWeight.normal),
+                      '200 contacts'),
                 ),
               ],
-            )),
-            SizedBox(
-              height: 400,
-              child: ListView.builder(
-                itemCount: myfriends.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    leading: CircleAvatar(
-                      backgroundColor: Colors.white,
-                      radius: 30,
-                      backgroundImage:
-                          NetworkImage(myfriends[index].image ?? ''),
-                    ),
-                    title: Text(myfriends[index].title ?? ''),
-                    subtitle: Text("few minutes ago"),
-                  );
-                },
-              ),
             ),
-          ],
-        ),
-      ),
-
-
+            actions: [
+              SizedBox(
+                  width: 50,
+                  child: Icon(
+                    Icons.search,
+                    color: Colors.white,
+                  )),
+          PopupMenuButton(
+                    icon: Icon(Icons.more_vert),
+                    itemBuilder: (context) => [
+                          PopupMenuItem(child: Text("Invite a friend")),
+                          PopupMenuItem(child: Text("Contacts")),
+                          PopupMenuItem(child: Text("Refresh")),
+                          PopupMenuItem(child: Text("Help")),
+                        ])
+            ],
+          ),
+      body: Center(
+            child: ListView.builder(
+                itemCount: contacts.length,
+                itemBuilder: (context,index){
+                  return ListTile(
+                    onTap: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=>personalsms(contacts: contacts[index])));
+                    },
+                    leading: (contacts[index].avatar!=null)! && contacts[index].avatar!.length>0?
+                    CircleAvatar(
+                      backgroundColor: Colors.black,
+                    ):
+                    CircleAvatar(
+                      child:  Text(contacts[index].initials()),
+                    ),
+                    title:  Text(contacts[index].displayName.toString()),
+                    subtitle: Text(contacts[index].phones!.isEmpty?'':contacts[index].phones![0].value!),
+                  );
+                }
+            ),
+          ),
     );
   }
+
+
+  // @override
+  // Widget build(BuildContext context) {
+  //  return Scaffold(
+  //     body: SingleChildScrollView(
+  //       child: Column(
+  //         children: [
+  //           SizedBox(
+  //               child: Column(
+  //             children: [
+  //               ListTile(
+  //                 leading: CircleAvatar(
+  //                   child: Icon(Icons.people),
+  //                 ),
+  //                 title: Text('New group'),
+  //                 onTap: () {
+  //                   Navigator.push(context,
+  //                       MaterialPageRoute(builder: (context) => contactno()));
+  //                 },
+  //               ),
+
+
+  //               ListTile(
+  //                 leading: CircleAvatar(
+  //                   child: Icon(Icons.person_add),
+  //                 ),
+  //                 title: Text('New contact'),
+  //                 onTap: () {
+  //                   Navigator.push(context, MaterialPageRoute(builder: (context)=>addnumber()));
+  //                 },
+  //               ),
+  //             ],
+  //           )),
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
 }
---
+++ lib/personal sms.dart
@@ -0,0 +1,289 @@
+import 'package:flutter/material.dart';
+import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
+import 'package:umang2/main.dart';
+
+
+class personalsms extends StatefulWidget {
+
+  final contacts;
+  const personalsms({required this.contacts,Key? key}) : super(key: key);
+
+  @override
+  State<personalsms> createState() => _personalsmsState();
+}
+
+class _personalsmsState extends State<personalsms> {
+  @override
+  Widget build(BuildContext context) {
+
+    FocusNode  messagefocuce = FocusNode();
+    TextEditingController message = TextEditingController();
+    TextEditingController textarea = TextEditingController();
+    TextEditingController emoji= TextEditingController();
+    late bool hideemoji = true;
+    FocusNode focusNode = FocusNode();
+    bool isShow = true;
+
+    @override
+    void initState() {
+      // TODO: implement initState
+      super.initState();
+
+      focusNode.addListener(() {
+        if (focusNode.hasFocus) {
+          setState(() {
+            hideemoji = true;
+          });
+        } else {
+          setState(() {
+            hideemoji = false;
+          });
+        }
+      });
+      textarea.addListener(() {
+        if (textarea.text != '') {
+          setState(() {
+            isShow = false;
+          });
+          setState(() {
+            isShow = false;
+          });
+        } else {
+          setState(() {
+            isShow = true;
+          });
+        }
+      });
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: CircleAvatar(
+          child: Text(widget.contacts.initials()),
+          radius: 35,
+          // backgroundColor: Colors.transparent,
+        ),
+        title: Column(
+          children: [
+            Text(widget.contacts.displayName.toString()),
+          ],
+        ),
+        actions: [
+          Row(
+            children: [
+              Row(
+                children: [
+                  IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.videocam)),
+                  IconButton(onPressed: () {}, icon: Icon(Icons.call)),
+                  PopupMenuButton(
+                      icon: Icon(Icons.more_vert),
+                      itemBuilder: (context) => [
+                        PopupMenuItem(child: Text("View contact")),
+                        PopupMenuItem(child: Text("Media, links and docs")),
+                        PopupMenuItem(child: Text("Search")),
+                        PopupMenuItem(child: Text("mute notifications")),
+                        PopupMenuItem(child: Text("Disappearing message")),
+                        PopupMenuItem(child: Text("Wallpaper")),
+                        PopupMenuItem(child:
+                        PopupMenuItem(child: Text('More'))),
+                      ])
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+        body: Container(
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
+            children: [
+              Image.network('https://mcdn.wallpapersafari.com/medium/27/32/jt4AoG.jpg',
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                fit: BoxFit.cover,
+              ),
+              ListView(),
+              Align(
+                  alignment: Alignment.bottomLeft,
+                  child: WillPopScope(
+                    onWillPop: () {
+                      if (hideemoji == true) {
+                        Navigator.pop(context);
+                      } else {
+                        setState(() {
+                          hideemoji = !hideemoji;
+                        });
+                      }
+                      return Future.value();
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Row(
+                          children: [
+                            Container(
+                              width: MediaQuery.of(context).size.width -50,
+                              child: Card(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(25)
+                                ),
+                                child: TextFormField(
+                                  onTap: () {},
+                                  keyboardType: TextInputType.multiline,
+                                  minLines: 1,
+                                  maxLines: 3,
+                                  controller: textarea,
+                                  textAlignVertical: TextAlignVertical.center,
+                                  focusNode: focusNode,
+                                  decoration: InputDecoration(
+                                    hintText: 'Message',
+                                    contentPadding:
+                                    EdgeInsets.only(left: 3, right: 3),
+                                    suffixIcon: Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        IconButton(
+                                            onPressed: () {
+                                              showModalBottomSheet(
+                                                  context: context,
+                                                  builder: (context) =>
+                                                      Bottommodal());
+                                            },
+                                            icon: Icon(Icons.attach_file)),
+                                        Visibility(
+                                          visible: isShow,
+                                          child: IconButton(
+                                            onPressed: () {
+                                              Navigator.push(context, MaterialPageRoute(builder: (context)=>Cameraapp()));
+
+                                            },
+                                            icon: Icon(Icons.camera_alt),
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                    prefixIcon: IconButton(
+                                      onPressed: () {
+                                        focusNode.unfocus();
+                                        focusNode.canRequestFocus = true;
+
+                                        setState(() {
+                                          hideemoji = !hideemoji;
+                                        });
+                                      },
+                                      icon: Icon(Icons.emoji_emotions),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Visibility(
+                                visible: isShow,
+                                replacement: CircleAvatar(
+                                  child: IconButton(
+                                    onPressed: () {},
+                                    icon: Icon(Icons.send),
+                                  ),
+                                ),
+                                child: CircleAvatar(
+                                  child: IconButton(
+                                    onPressed: () {},
+                                    icon: Icon(Icons.mic),
+
+                                  ),
+                                ))
+                          ],
+                        ),
+                        MyEmoji(),
+                      ],
+                    ),
+                  )),
+            ],
+          ),
+        )
+
+    );
+  }
+  Widget MyEmoji() {
+    return Container(
+      child: Offstage(
+        // offstage: hideemoji,
+        child: SizedBox(
+            width: MediaQuery.of(context).size.width -20,
+            height: 250,
+            child: EmojiPicker(
+              // textEditingController: message,
+            )),
+      ),
+    );
+  }
+  Widget Bottommodal() {
+    return Container(
+      height: 250,
+      //  width: MediaQuery.of(context).size.width,
+      child: Card(
+        margin: EdgeInsets.all(10),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 20),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  iconcreation(Icons.file_copy, Colors.indigo, 'Document'),
+                  SizedBox(
+                    width: 40,
+                  ),
+                  iconcreation(Icons.camera_alt, Colors.pink, 'Camera',),
+                  SizedBox(
+                    width: 40,
+                  ),
+                  iconcreation(Icons.insert_photo, Colors.purple, 'Gallery'),
+                ],
+              ),
+              SizedBox(width: 40,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  iconcreation(Icons.headset, Colors.orange, 'Audio'),
+                  SizedBox(
+                    width: 40,
+                  ),
+                  iconcreation(Icons.location_pin, Colors.green, 'Location'),
+                  SizedBox(
+                    width: 40,
+                  ),
+                  iconcreation(Icons.person, Colors.blue, 'Contact'),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget iconcreation(IconData icon, Color color, String text,) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 30,
+          backgroundColor: color,
+          child: Icon( icon,
+            size: 29,
+            color: Colors.white,
+          ),
+        ),
+        SizedBox(
+          height: 5,
+        ),
+        Text(text),
+      ],
+    );
+  }
+}
+
+
+
